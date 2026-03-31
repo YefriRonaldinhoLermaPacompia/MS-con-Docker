26ba4411b6fb35fd94500a957964dfac7afd37e4
Yefri Ronaldinho Lerma Pacompia <ronallerma033@gmail.com>
2026-03-31 13:25:55 -0500

    MS_configServer

--- ms-producto/src/main/java/com/example/producto/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.producto.repository;
-
-
-import com.example.producto.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
